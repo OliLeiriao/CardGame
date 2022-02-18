49640387f0e0d7fcbfe40d428700f64a86c6a7ba
Oliver <oliver@leiriao.net>
2022-02-18 14:46:08 -0500

    Implemented multiple rounds, score counter, player quitting mechanic

--- src/GUI.java
+++ src/GUI.java
@@ -1,70 +1,56 @@
 import javax.swing.*;
-import java.awt.GridLayout;
-import java.awt.BorderLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.util.*;
+import java.awt.*;
+import java.awt.event.*;
 
 public class GUI implements ActionListener {
 
-    private final JLabel label1;
-    public War war;
-    public Deck deck;
-    public List<Player> players;
-
-    public GUI(){
-
-        /**
-         * Game stuff:
-        
-        deck = new Deck();
-        players.add(new Player("Gab"));
-        players.add(new Player("Jo"));
-        war = new War(deck, players);
-        */
-
-
-        JFrame frame = new JFrame();
-
-        /**
-         * Button
-         */
-        JButton button = new JButton("War!");
-        button.addActionListener(this);
-
-        /**
-         * Label for text
-         */
-        label1 = new JLabel("Number of clicks: 0");
-
-        /**
-         * Panel parameters
-         */
-        JPanel panel = new JPanel();
-        panel.setBorder(BorderFactory.createEmptyBorder(300, 300, 100, 300));
-        panel.setLayout(new GridLayout(0,1));
-        panel.add(button);
-        panel.add(label1);
-
-        /**
-         * Frame parameters
-         */
-        frame.add(panel, BorderLayout.CENTER);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setTitle("Simple");
-        frame.pack();
-        frame.setVisible(true);
-
-    }
+    private final JLabel scoreboard;
 
     public static void main(String[] args){
         new GUI();
     }
 
+    public GUI(){
+
+        JFrame frame = new JFrame();
+
+        // Frame
+        frame.setVisible(true); // Makes frame visible
+
+        frame.setSize(900,600); // Sets size of frame
+
+        frame.setTitle("War"); // Sets Title of frame;
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Exits applications (Default is just HIDE_ON_CLOSE);
+
+        frame.setResizable(false); // Prevents resizing of frame
+
+        ImageIcon icon = new ImageIcon("assets/card-game-icon-25.png"); // Find icon file
+        frame.setIconImage(icon.getImage()); // Sets Icon for frame window
+
+        frame.getContentPane().setBackground(new Color(0xdedede)); // Changes frame background colour.
+
+        // Label (Display component, can have text, image or both
+        scoreboard = new JLabel();
+        scoreboard.setText("Score: \n"); // set text for label
+
+        // Panel (Container for other components, can also section off frames
+        JPanel panel = new JPanel();
+        panel.setBackground(new Color(0xaaaaaa));
+        panel.setBounds(25,150,175,200);
+
+        panel.add(scoreboard);
+
+
+        // Add our components to frame
+        frame.add(panel);
+
+
+
+    }
+
     @Override
     public void actionPerformed(ActionEvent event) {
-        
-        label1.setText("hi");
-
+        scoreboard.setText("hi");
     }
 }
